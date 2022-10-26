30fcfb87bdb7bcd0eb2e8778bb6c7f646f30b939
Tadas <tadas.zeimo@gmail.com>
2022-10-26 21:04:55 +0300

    case1 solved

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+function getResult(event) {
+    event.preventDefault();
+  
+    const kg = Number(document.getElementById("search").value);
+    let lb = kg * 2.2046;
+    let g = kg / 0.001;
+    let oz = kg * 35.274;
+  
+    const output = document.getElementById("output");
+    output.innerHTML = "";
+    output.style.marginLeft = "10%";
+    output.style.color = "#f58a42";
+  
+    const h1 = document.createElement("h1");
+    h1.textContent = `Svarai(lb) = ${lb}`;
+  
+    const h2 = document.createElement("h1");
+    h2.textContent = `Gramai(g) = ${g}`;
+  
+    const h3 = document.createElement("h1");
+    h3.textContent = `Uncijos(oz) = ${oz}`;
+  
+    output.append(h1, h2, h3);
+  };
+  
+  document.querySelector("form").addEventListener("submit", getResult);
